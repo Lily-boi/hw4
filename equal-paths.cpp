dcd1af23ccc930d351cf4916952aef5e2db6ba35
Lewis O-Schneider <loschnei@usc.edu>
2025-04-09 21:51:02 +0000

    equal-paths part 1 done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,31 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+bool checkDepth(Node * node, int depth, int& leaf_depth) {
+  if (node == nullptr) {
+    return true;
+  }
+  if (node->left == nullptr && node->right == nullptr) {
+    if (leaf_depth == -1) {
+      leaf_depth = depth;
+    }
+    return depth == leaf_depth;
+  }
+  
 
+  return (checkDepth(node->left, depth + 1, leaf_depth) &&
+         checkDepth(node->right, depth + 1, leaf_depth));
+
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+  if (root == nullptr) {
+    return false;
+  }
+  
+  int leaf_depth = -1;
+  return checkDepth(root, 0, leaf_depth);
 
 }
 
